806738044b0a4288a21317378a9329f157a17418
hahahellooo <hahahello777@gmail.com>
2024-10-16 18:01:50 +0900

    chage people

--- src/main/java/shop/samdul/greeting/controller/GreetingController.java
+++ src/main/java/shop/samdul/greeting/controller/GreetingController.java
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import shop.samdul.greeting.People;
-
 import java.util.Stack;
 
 @Controller
